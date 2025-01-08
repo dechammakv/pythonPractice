0b3056439a3555f7d84d61aa62752e2746d8149b
dechammakv <dechammakarthamada8@gmail.com>
2025-01-08 16:42:51 +0530

    Initial Commit

---
+++ Basics.py
@@ -0,0 +1,11 @@
+print('Hello Everyone')
+
+def message():
+    print('Procedural way of writing: Hello')
+message()
+
+class Demo:
+    def instance_method(self):
+        print('Object oriented way of printing: Hello')
+d1 = Demo()
+d1.instance_method()
---
+++ VariablesDataTypes/Overview.py
@@ -0,0 +1,3 @@
+#1. Python is dynamically typed programming language
+#2. In Python while decalring variable we no need to  mention Data Types
+#3. Data Types will be assinged by language based on value stored in the variable
